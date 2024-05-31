400257c61b818efb4871e89902f1f888639cae3c
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2024-06-01 03:37:27 +0900

    fix: 리사이징 로직 삭제

--- routes.py
+++ routes.py
@@ -2,7 +2,6 @@ from dotenv import load_dotenv
 from fastapi import APIRouter
 from openai import OpenAI
 import os
-import requests
 
 router = APIRouter(prefix="/api/v1")
 
@@ -12,29 +11,21 @@ api_key = os.getenv("OPENAI_API_KEY")
 
 
 @router.post("/generate_concepts")
-async def gpt_api(message):
+async def gpt_api(topic):
     client = OpenAI(api_key=api_key)
     completion = client.chat.completions.create(
         model="gpt-3.5-turbo-16k",
         messages=[
-            {"role": "system", "content": "You will be asked to enter some topic from the user." 
-                                          "All you have to do is recommend four logo design concepts that fit that theme." 
+            {"role": "system", "content": "You will be asked to enter some topic from the user."
+                                          "Recommend 4 logo design concepts that fit the topic and what the concept means as content."
+                                          "In concept, list as nouns what elements go into the design."
                                           "In Korean."},
-            {"role": "user", "content": message}
+            {"role": "user", "content": topic}
         ]
     )
     return completion.choices[0].message
 
-def download_image(image_url, save_path):
-    response = requests.get(image_url)
-    if response.status_code == 200:
-        with open(save_path, 'wb') as file:
-            file.write(response.content)
-        print(f'Image successfully downloaded: {save_path}')
-    else:
-        print('Failed to download image')
-
-@router.post("/generate_logo")
+@router.post("/generate_logos")
 async def dalle_api(message):
     client = OpenAI(api_key=api_key)
     logo_urls = []
@@ -48,9 +39,5 @@ async def dalle_api(message):
     # 생성된 이미지의 URL 추출
     logo_url = str(url_response.data[0].url)  # 이미지 url 출력
     logo_urls.append(logo_url)
-
-    image_url = logo_url
-    save_path = 'images/test2.png'  # 예: 'downloaded_image.jpg'
-    download_image(image_url, save_path)
 
     return logo_urls
